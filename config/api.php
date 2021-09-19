a1a12201175296a4b7205a1658a37546dfe6cf6a
Evan Hsu <evanhsu@gmail.com>
2021-09-19 12:42:05 -0600

    Remove Fractal transformer function from api.php because it was breaking the artisan config:cache command

--- config/api.php
+++ config/api.php
@@ -200,12 +200,12 @@ return [
     */
 
     // 'transformer' => env('API_TRANSFORMER', Dingo\Api\Transformer\Adapter\Fractal::class),
-    'transformer' => new Dingo\Api\Transformer\Adapter\Fractal(
-        (new League\Fractal\Manager)->setSerializer(new League\Fractal\Serializer\JsonApiSerializer()),
-        'include',
-        ',',
-        false
-    ),
+    // 'transformer' => new Dingo\Api\Transformer\Adapter\Fractal(
+    //     (new League\Fractal\Manager)->setSerializer(new League\Fractal\Serializer\JsonApiSerializer()),
+    //     'include',
+    //     ',',
+    //     false
+    // ),
 
     /*
     |--------------------------------------------------------------------------
