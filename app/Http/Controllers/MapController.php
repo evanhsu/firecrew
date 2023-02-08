189ec54dfe468f335d174f388af262b5cecff5a8
Evan Hsu <evanhsu@gmail.com>
2023-02-07 19:30:41 -0700

    The /map route now points to the NEW map page

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -4,25 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-Class MapController extends Controller
-{
-    public function getMap(Request $request)
-    {
-        // Display the main map page.
+Class MapController extends Controller {
+
+    public function getMap(Request $request) {
         // The menubar will be selected by the MenubarComposer (app/Http/ViewComposers/MenubarComposer.php)
         $request->session()->flash('active_menubutton', 'map'); // Tell the menubar which button to highlight
         return view('map');
-    } // End getMap()
-
-    public function getMap2(Request $request)
-    {
-        // The menubar will be selected by the MenubarComposer (app/Http/ViewComposers/MenubarComposer.php)
-        $request->session()->flash('active_menubutton', 'map'); // Tell the menubar which button to highlight
-        return view('map2');
     }
 
-    public function getMapJSON()
-    {
+    public function getMapJSON() {
         /**
          *   This will return a JSON array of all statusable features with their attached location data & contact info
          *
@@ -42,5 +32,5 @@ Class MapController extends Controller
             'crew_id' => '1',
             'latitude' => '42.67893',
             'longitude' => '-123.89409'))));
-    } // End getMapJSON()
+    }
 }
--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -5,59 +5,13 @@
 
 @section('stylesheets')
     @parent
-    <link rel="stylesheet" href="https://js.arcgis.com/3.14/esri/css/esri.css">
+    <link rel="stylesheet" href="https://js.arcgis.com/4.25/@arcgis/core/assets/esri/themes/light/main.css">
 @endsection
-
-
-@section('scripts-preload')
-    @parent
-    <script>
-        // Tell Dojo where to find custom modules for the ArcGIS API
-        var dojoConfig = {
-            locale: "en",
-            packages: [{
-                name: "js",
-                location: "/js"
-            }]
-        };
-    </script>
-@endsection
-
 
 @section('content')
-    <div id="content" class="container-fluid">
-        <div id="mapDiv">
-        <!-- ArcMap gets placed here -->
-        </div> <!-- /mapDiv -->
-        <div id="legendContainer">
-            <div id="legendDiv"></div>
-        </div>
-        <button type="button" class="btn btn-default" id="legendToggler">
-            <span id="left-arrow" class="glyphicon glyphicon-triangle-left" style="display:none"></span>
-            <span id="right-arrow" class="glyphicon glyphicon-triangle-right"></span>
-            Layers
-        </button>
-    </div>
+    <div id="map-root"></div>
 @endsection
 
 @section('scripts-postload')
-    @parent
-    <script src="https://js.arcgis.com/3.14/init.js"></script>  <?php /* ArcGIS API - must load before other classes */?>
-    {{--<script src="/js/esri/init.js"></script>--}}
-    <script>
-        $(document).ready(function() {
-            $('#legendToggler').click(function() {
-                $('#legendDiv').toggle("fast");
-                $('#left-arrow').toggle();
-                $('#right-arrow').toggle();
-            });
-            $('#legendDiv').click(function() {
-                $('#legendDiv').toggle("fast");
-                $('#left-arrow').toggle();
-                $('#right-arrow').toggle();
-            });
-        });
-
-    </script>
-    <script src="{{ asset('/js/localmap.js') }}"></script>
+    <script src="{{ mix('/js/map/bundle.js') }}"></script>
 @endsection
