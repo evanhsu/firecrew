da8f283afb1234382cf6b47a847c1ae6ef780572
Evan Hsu <evanhsu@gmail.com>
2023-03-13 00:44:44 -0600

    Added MethodNotAllowedHttpException to the BugSnag discard_classes array

--- config/bugsnag.php
+++ config/bugsnag.php
@@ -1,8 +1,10 @@
 <?php
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return [
     'discard_classes' => [
+        MethodNotAllowedHttpException::class,
         NotFoundHttpException::class,
     ],
 ];
